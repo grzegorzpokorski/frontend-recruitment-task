72daeb6f6969be206d1316887af5951a01f0ab56
Grzegorz Pokorski <mr.pokorski@gmail.com>
2023-02-21 11:26:44 +0100

    init after merge with new version

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!****************************!*\
+  !*** ./src/scss/main.scss ***!
+  \****************************/
+// extracted by mini-css-extract-plugin
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
